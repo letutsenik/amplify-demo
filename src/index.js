0d01061421be49eae48d7f5f0c37da47626eea44
dmitri_lipski <dzmitri.lipski@gmail.com>
2020-02-02 20:39:16 +0300

    Add prod env

--- src/index.js
+++ src/index.js
@@ -4,9 +4,15 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import awsmobile from "./awsmobile";
+
 import Amplify from 'aws-amplify'
 import config from './aws-exports'
+
+console.log("awsmobile", awsmobile)
 Amplify.configure(config);
+
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
