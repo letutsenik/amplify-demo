c653fd717ab1d54d75707b23debd6b7ecd272784
dmitri_lipski <dzmitri.lipski@gmail.com>
2020-02-02 17:26:35 +0300

    Inital commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,34 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { Auth, Hub } from 'aws-amplify';
+
+function checkUser() {
+  Auth.currentAuthenticatedUser()
+    .then(user => console.log({ user }))
+    .catch(err => console.log(err))
+}
+
+function signOut() {
+  Auth.signOut()
+    .then(data => console.log(data))
+    .catch(err => console.log(err))
+}
+
 function App() {
+  useEffect(() => {
+    Hub.listen('auth', (data) => {
+      const { payload } = data
+      console.log('A new auth event has happened: ', data)
+      if (payload.event === 'signIn') {
+        console.log('a user has signed in!')
+      }
+      if (payload.event === 'signOut') {
+        console.log('a user has signed out!')
+      }
+    })
+  }, [])
   return (
     <div className="App">
       <header className="App-header">
@@ -10,14 +36,16 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={() => Auth.federatedSignIn()}>Sign In</button>
+        <button
+          onClick={() => Auth.federatedSignIn({ provider: 'Facebook' })}
+        >Sign In with Facebook</button>
+
+        <button
+          onClick={() => Auth.federatedSignIn({ provider: 'Google' })}
+        >Sign In with Google</button>
+        <button onClick={checkUser}>Check User</button>
+        <button onClick={signOut}>Sign Out</button>
       </header>
     </div>
   );
